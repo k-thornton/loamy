4059eee0afd51bb8929f13f0f743aaa46f2f28a6
Kyle Thornton <kyle@ration.fyi>
2024-03-22 23:11:52 -0700

    pushed the callouts in a smidge

--- frontend/src/components/Callout.js
+++ frontend/src/components/Callout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Callout = ({ children }) => {
   return (
-    <aside className="card bg-secondary p-4 mb-6">{children}</aside>
+    <aside className="card bg-secondary p-4 mb-6 mx-3">{children}</aside>
   );
 };
 
