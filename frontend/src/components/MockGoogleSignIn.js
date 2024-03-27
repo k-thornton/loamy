e15741dd7e493c1a6466a181e88093eb0ecb8abc
Kyle Thornton <kyle@ration.fyi>
2024-03-26 23:19:41 -0700

    small tweak to a div to get it to look good on large screens

--- frontend/src/components/MockGoogleSignIn.js
+++ frontend/src/components/MockGoogleSignIn.js
@@ -7,7 +7,7 @@ const MockGoogleSignIn = () => {
     <>
       <div className="hero min-h-screen bg-base-100">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left pl-4 pt-5">
+          <div className="text-center lg:text-left pl-4 pt-5 min-w-[40vh]">
             <h1 className="text-5xl font-bold">Sign In.</h1>
             <p className="py-6 min-w-full">
               Welcome to Loamy!  Please sign in to begin.
